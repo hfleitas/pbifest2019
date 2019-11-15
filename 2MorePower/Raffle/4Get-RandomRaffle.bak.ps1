552d4b7d35e0c0aef5df4170a12ba49750e90f36
Hiram Fleitas <hiramfleitas@hotmail.com>
2019-11-15 11:53:31 -0800

    fixed raffle

--- 2MorePower/Raffle/4Get-RandomRaffle.bak.ps1
+++
@@ -1,3 +0,0 @@
-$Evals = $(foreach ($line in Get-Content "C:\fleitasarts\pbifest2019\2MorePower\Raffle\2Eval.txt") {$line.tolower().split(" ")}) | sort | Get-Unique
-echo $Evals > "C:\fleitasarts\pbifest2019\2MorePower\Raffle\unique-sorted.txt"
-Get-Random -Input $Evals -Count 2 > "C:\fleitasarts\pbifest2019\2MorePower\Raffle\Winners.txt"
